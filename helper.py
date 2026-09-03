aab2a0dbf38d1aaee913b3d2346562b4b00dc8b8
Petr Sabanov <pasabanov@murena.io>
2026-09-04 02:50:21 +0300

    Initial commit

---
+++ helper.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+from pathlib import Path
+
+
+ARCHITECTURES = {
+	'x86_64': {
+		'gcc': {
+			'compiler': 'x86_64-linux-gnu-g++',
+			'objdump': 'x86_64-linux-gnu-objdump',
+		},
+		'clang': {
+			'compiler': 'clang++',
+			'objdump': 'llvm-objdump',
+		},
+	},
+	'rv64gv': {
+		'gcc': {
+			'compiler': 'riscv64-linux-gnu-g++',
+			'objdump': 'riscv64-linux-gnu-objdump',
+		},
+		'clang': {
+			'compiler': 'clang++',
+			'objdump': 'llvm-objdump',
+		},
+	},
+}
+
+PROFILES = [
+	'O0',
+	'O2',
+	'O3',
+	'Ofast',
+]
+
+OPENMP_OPTIONS = [
+	False,
+	True,
+]
+
+COMPILERS = [
+	'gcc',
+	'clang',
+]
+
+
+def run(command):
+	print(' '.join(command), flush=True)
+	subprocess.check_call(command)
+
+
+def build(arch, compiler, profile, openmp):
+	config_name = f'{arch}-{compiler}-{profile}'
+
+	if openmp:
+		config_name += '-openmp'
+
+	build_dir = Path('build') / config_name
+
+	toolchain = ARCHITECTURES[arch][compiler]
+
+	run([
+		'cmake',
+		'-S', '.',
+		'-B', str(build_dir),
+		f'-DCMAKE_CXX_COMPILER={toolchain["compiler"]}',
+		f'-DCMAKE_OBJDUMP={toolchain["objdump"]}',
+		f'-DALFI_ARCH={arch}',
+		f'-DALFI_COMPILER={compiler}',
+		f'-DALFI_PROFILE={profile}',
+		f'-DALFI_OPENMP={"ON" if openmp else "OFF"}',
+	])
+
+	run([
+		'cmake',
+		'--build',
+		str(build_dir),
+		'-j',
+	])
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+	'--arch',
+	choices=[*ARCHITECTURES, 'all'],
+	default='all',
+)
+
+parser.add_argument(
+	'--compiler',
+	choices=[*COMPILERS, 'all'],
+	default='all',
+)
+
+parser.add_argument(
+	'--profile',
+	choices=[*PROFILES, 'all'],
+	default='all',
+)
+
+parser.add_argument(
+	'--openmp',
+	choices=['on', 'off', 'both'],
+	default='both',
+)
+
+args = parser.parse_args()
+
+
+architectures = list(ARCHITECTURES) if args.arch == 'all' else [args.arch]
+compilers = COMPILERS if args.compiler == 'all' else [args.compiler]
+profiles = PROFILES if args.profile == 'all' else [args.profile]
+openmp_options = OPENMP_OPTIONS if args.openmp == 'both' else [args.openmp == 'on']
+
+
+for arch in architectures:
+	for compiler in compilers:
+		for profile in profiles:
+			for openmp in openmp_options:
+				build(arch, compiler, profile, openmp)
---
+++ main.cpp
@@ -0,0 +1,94 @@
+#include "ALFI/ALFI/ALFI.h"
+
+//// SPLINES
+// Cubic
+template class alfi::spline::CubicSpline<double>;
+
+// Linear
+template class alfi::spline::LinearSpline<double>;
+
+// PolyEqv
+template class alfi::spline::PolyEqvSpline<double>;
+
+// Quadratic
+template class alfi::spline::QuadraticSpline<double>;
+
+// Step
+template class alfi::spline::StepSpline<double>;
+
+//// DIST
+template alfi::DefaultContainer<double> alfi::dist::uniform(alfi::SizeT n, const double& a, const double& b);
+template alfi::DefaultContainer<double> alfi::dist::quadratic(alfi::SizeT n, const double& a, const double& b);
+template alfi::DefaultContainer<double> alfi::dist::cubic(alfi::SizeT n, const double& a, const double& b);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev(alfi::SizeT n, const double& a, const double& b);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_stretched(alfi::SizeT n, const double& a, const double& b);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_augmented(alfi::SizeT n, const double& a, const double& b);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_2(alfi::SizeT n, const double& a, const double& b);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_3(alfi::SizeT n, const double& a, const double& b);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_3_stretched(alfi::SizeT n, const double& a, const double& b);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_4(alfi::SizeT n, const double& a, const double& b);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_4_stretched(alfi::SizeT n, const double& a, const double& b);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_ellipse(alfi::SizeT n, const double& a, const double& b, const double& ratio);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_ellipse_stretched(alfi::SizeT n, const double& a, const double& b, const double& ratio);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_ellipse_augmented(alfi::SizeT n, const double& a, const double& b, const double& ratio);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_ellipse_2(alfi::SizeT n, const double& a, const double& b, const double& ratio);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_ellipse_3(alfi::SizeT n, const double& a, const double& b, const double& ratio);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_ellipse_3_stretched(alfi::SizeT n, const double& a, const double& b, const double& ratio);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_ellipse_4(alfi::SizeT n, const double& a, const double& b, const double& ratio);
+template alfi::DefaultContainer<double> alfi::dist::chebyshev_ellipse_4_stretched(alfi::SizeT n, const double& a, const double& b, const double& ratio);
+template alfi::DefaultContainer<double> alfi::dist::logistic(alfi::SizeT n, const double& a, const double& b, const double& steepness);
+template alfi::DefaultContainer<double> alfi::dist::logistic_stretched(alfi::SizeT n, const double& a, const double& b, const double& steepness);
+template alfi::DefaultContainer<double> alfi::dist::erf(alfi::SizeT n, const double& a, const double& b, const double& steepness);
+template alfi::DefaultContainer<double> alfi::dist::erf_stretched(alfi::SizeT n, const double& a, const double& b, const double& steepness);
+
+//// MISC
+template alfi::DefaultContainer<double> alfi::misc::barycentric(
+	const alfi::DefaultContainer<double>& X,
+	const alfi::DefaultContainer<double>& Y,
+	const alfi::DefaultContainer<double>& xx,
+	alfi::dist::Type dist_type,
+	const double& epsilon);
+
+//// POLY
+template double alfi::poly::val(const alfi::DefaultContainer<double>& coeffs, const double& x);
+template alfi::DefaultContainer<double> alfi::poly::val(const alfi::DefaultContainer<double>& coeffs, const alfi::DefaultContainer<double>& x);
+
+template alfi::DefaultContainer<double> alfi::poly::lagrange<double>(
+	const alfi::DefaultContainer<double>& X,
+	const alfi::DefaultContainer<double>& Y);
+
+template alfi::DefaultContainer<double> alfi::poly::lagrange_vals<double>(
+	const alfi::DefaultContainer<double>& X,
+	const alfi::DefaultContainer<double>& Y,
+	const alfi::DefaultContainer<double>& xx);
+
+template alfi::DefaultContainer<double> alfi::poly::imp_lagrange<double>(
+	const alfi::DefaultContainer<double>& X,
+	const alfi::DefaultContainer<double>& Y);
+
+template alfi::DefaultContainer<double> alfi::poly::imp_lagrange_vals<double>(
+	const alfi::DefaultContainer<double>& X,
+	const alfi::DefaultContainer<double>& Y,
+	const alfi::DefaultContainer<double>& xx,
+	const double& epsilon);
+
+template alfi::DefaultContainer<double> alfi::poly::newton<double>(
+	const alfi::DefaultContainer<double>& X,
+	const alfi::DefaultContainer<double>& Y);
+
+template alfi::DefaultContainer<double> alfi::poly::newton_vals<double>(
+	const alfi::DefaultContainer<double>& X,
+	const alfi::DefaultContainer<double>& Y,
+	const alfi::DefaultContainer<double>& xx);
+
+//// RATF
+template double alfi::ratf::val_mul(const alfi::ratf::RationalFunction<double>& rf, const double& x);
+template alfi::DefaultContainer<double> alfi::ratf::val_mul(const alfi::ratf::RationalFunction<double>& rf, const alfi::DefaultContainer<double>& xx);
+
+template double alfi::ratf::val_div(const alfi::ratf::RationalFunction<double>& rf, const double& x);
+template alfi::DefaultContainer<double> alfi::ratf::val_div(const alfi::ratf::RationalFunction<double>& rf, const alfi::DefaultContainer<double>& xx);
+
+template double alfi::ratf::val(const alfi::ratf::RationalFunction<double>& rf, const double& x);
+template alfi::DefaultContainer<double> alfi::ratf::val(const alfi::ratf::RationalFunction<double>& rf, const alfi::DefaultContainer<double>& xx);
+
+template alfi::ratf::RationalFunction<double> alfi::ratf::pade(alfi::DefaultContainer<double> P, alfi::SizeT n, alfi::SizeT m, const double& epsilon);
